f83bdaf2f86dbbcf407369835addd0cbcafd2909
Mats Løvstrand Berntsen <matslovstrandberntsen@outlook.com>
2018-10-08 15:37:39 +0200

    video 12

--- RotatingTriangle/src/Application.cpp
+++ RotatingTriangle/src/Application.cpp
@@ -115,6 +115,10 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Awesome triangle", NULL, NULL);
@@ -126,6 +130,8 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
 
 	if (glewInit() != GLEW_OK) {
 		std::cout << "Error!" << std::endl;
@@ -143,18 +149,24 @@ int main(void)
 		0, 2, 3,
 	};
 
+	unsigned int vao;
+	GLCall( glGenVertexArrays(1, &vao) );
+	GLCall(glBindVertexArray(vao) );
+
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	GLCall(glEnableVertexAttribArray(0));
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0));
 
 	unsigned int ibo;
 	glGenBuffers(1, &ibo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indicies, GL_STATIC_DRAW);
 
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
+
 
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
@@ -166,13 +178,37 @@ int main(void)
 	ASSERT(location != -1);
 	GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
 
+
+	GLCall(glBindVertexArray(0))
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+
+	float r = 0.0f;
+	float increment = 0.05f;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		GLCall(glUseProgram(shader));
+		
+		GLCall(glUniform4f(location, r, 0.2f, 0.4f, 1.0f));
+
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+		if (r > 1.0f)
+		increment = -0.05f;
+		else if (r < 0.0f)
+			increment = 0.05f;
+		
+		r += increment;
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
